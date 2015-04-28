1b3a54f0e9ae8532821772221a1fdfd2df9e84ac
JorgeManzano125 <capi-jorge_bus@hotmail.com>
2015-04-28 19:05:40 +0200

    first commnit

---
+++ Paciente/src/paciente/Paciente.java
@@ -0,0 +1,14 @@
+package paciente;
+
+public class Paciente {
+	private String nombre;
+	private String estado;
+	public Paciente(String nombre, String estado) {
+		this.nombre = nombre;
+		this.estado = estado;
+	}
+	
+	
+	
+	
+}
